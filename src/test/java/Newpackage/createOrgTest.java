24f7a0edaf4d9e994493315d7c19f25801c9f8f5
HP <laxmidevammabelgal@gmail.com>
2026-04-30 14:44:08 +0530

    new commit for practice

---
+++ src/test/java/Newpackage/createOrgTest.java
@@ -0,0 +1,33 @@
+package Newpackage;
+
+import org.testng.annotations.Test;
+
+public class createOrgTest {
+	@Test
+	public void createOrg() {
+		System.out.println("org test cretaed success");
+	}
+	
+	
+	@Test
+	public void createOrgwithDate() {
+		System.out.println("org test cretaed success");
+	}
+
+	@Test
+	public void createOrgurrentPositrion() {
+		System.out.println("org test cretaed success");
+	}
+
+	@Test
+	public void createOrgwithLocation() {
+		System.out.println("org test cretaed success");
+	}
+
+	@Test
+	public void createOrgwithPlace() {
+		System.out.println("org test cretaed success");
+	}
+
+
+}
