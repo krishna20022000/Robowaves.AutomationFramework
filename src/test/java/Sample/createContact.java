a98242de92fd9dd0ce6e8b431c0dfa0013dddc38
HP <laxmidevammabelgal@gmail.com>
2026-04-30 14:10:37 +0530

    updated commit

---
+++ src/test/java/Sample/createContact.java
@@ -0,0 +1,18 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class createContact {
+	
+	@Test
+	public void createContactTest() {
+		System.out.println("create contact success");
+	}
+	
+	@Test
+	public void createcontactwithdate() {
+		System.out.println("created success");
+	}
+	
+
+}
---
+++ src/test/java/Sample/createcontactTest.java
@@ -0,0 +1,20 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class createcontactTest {
+	
+		@Test
+		public void CreateContact() {
+			System.out.println("contact created");
+		}
+		
+		@Test
+		public void CreateContactwithDtae() {
+			System.out.println("contact with date created");
+		}
+		
+		
+	}
+
+
